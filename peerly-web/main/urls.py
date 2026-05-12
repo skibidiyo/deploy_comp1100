5da3f009ffd9f6285052edd85652f2966b9b8ace
skibidiyo <diyobanu@gmail.com>
2026-05-12 18:40:46 +1000

    added onboarding

--- peerly-web/main/urls.py
+++ peerly-web/main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import landing_page
+from .views import landing_page, onboarding_step1, onboarding_step2, onboarding_step3, onboarding_step4, onboarding_step5
 
 urlpatterns = [
     path('', landing_page, name='landing-page'),
+    path('onboarding/step1/', onboarding_step1, name='onboarding-step1'),
+    path('onboarding/step2/', onboarding_step2, name='onboarding-step2'),
+    path('onboarding/step3/', onboarding_step3, name='onboarding-step3'),
+    path('onboarding/step4/', onboarding_step4, name='onboarding-step4'),
+    path('onboarding/step5/', onboarding_step5, name='onboarding-step5'),
 ]
--- peerly-web/main/views.py
+++ peerly-web/main/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def landing_page(request):
 	return render(request, 'main/landing.html')
+
+def onboarding_step1(request):
+	return render(request, 'main/onboarding.html')
+
+def onboarding_step2(request):
+	return render(request, 'main/onboarding_step2.html')
+
+def onboarding_step3(request):
+	return render(request, 'main/onboarding_step3.html')
+
+def onboarding_step4(request):
+	return render(request, 'main/onboarding_step4.html')
+
+def onboarding_step5(request):
+	return render(request, 'main/onboarding_step5.html')
